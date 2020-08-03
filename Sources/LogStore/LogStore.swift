bc477079fb5145bcecba7f958317d24763420217
rainkinz <brendan.grainger@gmail.com>
2020-08-03 18:37:04 +1200

    initial version

--- Sources/LogStore/LogStore.swift
+++ Sources/LogStore/LogStore.swift
@@ -1,3 +1,17 @@
+//
+//  LogStore.swift
+//  LogStoreDevelopment
+//
+//  Created by Brendan on 8/3/20.
+//  Copyright © 2020 Brendan. All rights reserved.
+//
+
 struct LogStore {
-    var text = "Hello, World!"
+    static var log: [String] = []
+}
+
+func printLog(_ string: String) {
+    print(string)
+    
+    LogStore.log.append(string)
 }
